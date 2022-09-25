7f6832ae8bfa50b54ff42d07575aa7e1e5fa9668
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2022-09-25 18:10:56 +0300

    pycharm_open

---
+++ hometask2/main.py
@@ -0,0 +1,16 @@
+import pkg_resources
+import re
+
+result = []
+#123
+
+def get_dependencies(package_name):
+    package = pkg_resources.working_set.by_key[package_name]
+    for r in package.requires():
+        r = "".join(c for c in str(r) if (c.isalpha() or c == '-'))
+        result.append(package_name + " -> " + r)
+        get_dependencies(r)
+
+
+get_dependencies("matplotlib")
+print(result)
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-package_json = requests.get('https://pypi.org/pypi/matplotlib/3.6.0/json', verify=False).json()
-print(package_json['info']['requires_dist'])
