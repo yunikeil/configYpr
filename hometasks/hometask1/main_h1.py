eb8dfa6394d50554489de7f7862c3eb143304fc9
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2022-09-30 21:03:16 +0300

    pycharm_open

---
+++ hometasks/hometask1/main_h1.py
@@ -0,0 +1,27 @@
+import sys
+import time
+import zipfile
+
+
+def main():
+    print(len(sys.argv))
+
+    if len(sys.argv) == 2:
+        pass
+    else:
+        print("Invalid file system!")
+
+
+    # file_system = sys.argv[1]
+
+    '''start_message = "Loading...\n\nWelcome to Hometask 01\n"
+    pre_name = "[root@localhost ~]# "
+    command = ""
+    
+    print(start_message)
+    time.sleep(1)
+    i = input(pre_name)
+    '''
+
+
+main()
