8149efa5efb75771c565e1cdaa052a5ef23b178a
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2022-10-13 20:10:11 +0300

    разделил на 2 if

--- hometasks/hometask1/test_main_h1.py
+++ hometasks/hometask1/test_main_h1.py
@@ -4,32 +4,34 @@ import socket
 import zipfile
 
 
-def pwd(command):
+class Commands(object):
+    def pwd(self, command):
+        pass
+
+    def ls(self, command):
+        pass
+
+    def cd(self, command):
+        pass
+
+    def cat(self, command):
+        pass
     pass
 
 
-def ls(command):
+class VShell(Commands):
+    @staticmethod
+    def startExpectationCommand():
+        command = str(input(pre_name))
     pass
 
 
-def cd(command):
-    pass
+shell = VShell()
+# commands = {'pwd':pwd, 'ls':ls, 'cd':cd, 'cat':cat}
+commands = [["pwd", shell.pwd], ["ls", shell.ls], ["cd", shell.cd], ["cat", shell.cat]]  # commands[0][1]("123")
 
-
-def cat(command):
-    pass
-
-
-#commands = ["pwd", "ls", "cd", "cat", "exit", "clear", "help"]
-#commands = {'pwd':pwd, 'ls':ls, 'cd':cd, 'cat':cat}
-commands = [["pwd", pwd], ["ls", ls], ["cd", cd], ["cat", cat]]  # commands[0][1]("123")
-
-
-if len(sys.argv) == 2 and zipfile.is_zipfile(sys.argv[1]):
-    path_file_system = sys.argv[1]
-else:
-    print("Invalid file system!")
-    sys.exit(1)
+if len(sys.argv) == 2 and zipfile.is_zipfile(sys.argv[1]): path_file_system = sys.argv[1]
+else: print("Invalid file system!"); sys.exit(1)
 
 system = sys.platform
 start_message = "Loading...\n\nWelcome to Hometask 01\n"
@@ -37,5 +39,10 @@ pre_name = f"[yunik@{socket.gethostname()} ~]# "
 
 print(start_message)
 current_path = ['root']
+
+
+
+
+
 
 
